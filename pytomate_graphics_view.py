ba99d2cf67e1157be1b67b17477497c0268d4652
Prashant Shukla <thesprashant@gmail.com>
2024-10-31 01:35:17 +0530

    Added zoom in and zoom out by mouse scroll wheel along with zoom clamp setting
    Signed-off-by: Prashant Shukla <thesprashant@gmail.com>

--- pytomate_graphics_view.py
+++ pytomate_graphics_view.py
@@ -12,6 +12,13 @@ class OurQGraphicsView(QGraphicsView):
 
         self.initui()
 
+        self.zoomInFactor = 1.25
+        self.zoomOutFactor = 1 / self.zoomInFactor
+        self.zoom = 10
+        self.zoomClamp = True
+        self.zoomStep = 1
+        self.zoomRange = [0, 10]
+
         self.setScene(self.graphicsScene)
     def initui(self):
         self.setRenderHints(QPainter.Antialiasing | QPainter.HighQualityAntialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
@@ -21,15 +28,25 @@ class OurQGraphicsView(QGraphicsView):
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+
     def mousePressEvent(self, event):
         if event.button() == Qt.MiddleButton:
             self.middleMouseButtonPress(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonPress(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonPress(event)
         else:
             super().mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MiddleButton:
             self.middleMouseButtonRelease(event)
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonRelease(event)
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonRelease(event)
         else:
             super().mouseReleaseEvent(event)
 
@@ -47,3 +64,32 @@ class OurQGraphicsView(QGraphicsView):
                                 Qt.LeftButton, event.buttons() & -Qt.LeftButton, event.modifiers())
         super().mousePressEvent(fakeEvent)
         self.setDragMode(QGraphicsView.NoDrag)
+
+    def leftMouseButtonPress(self, event):
+        super().mousePressEvent(event)
+
+    def rightMouseButtonPress(self, event):
+        super().mousePressEvent(event)
+
+    def leftMouseButtonRelease(self, event):
+        super().mousePressEvent(event)
+
+    def rightMouseButtonRelease(self, event):
+        super().mousePressEvent(event)
+
+    def wheelEvent(self, event):
+
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor
+            self.zoom += self.zoomStep
+        else:
+            zoomFactor = self.zoomOutFactor
+            self.zoom -= self.zoomStep
+
+        clamped = False
+        if self.zoom < self.zoomRange[0]: self.zoom, clamped = self.zoomRange[0], True
+        if self.zoom > self.zoomRange[1]: self.zoom, clamped = self.zoomRange[1], True
+
+        if not clamped or self.zoomClamp is False:
+            self.scale(zoomFactor, zoomFactor)
+
