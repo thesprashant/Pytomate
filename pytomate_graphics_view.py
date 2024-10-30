973d106ee286b4c27875052f8d4b69f09cd635ae
Prashant Shukla <thesprashant@gmail.com>
2024-10-31 00:52:22 +0530

    Added Middle Mouse Scrolling in Graphics View
    Signed-off-by: Prashant Shukla <thesprashant@gmail.com>

--- pytomate_graphics_view.py
+++ pytomate_graphics_view.py
@@ -1,6 +1,8 @@
+from platform import release
+
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtWidgets import *
 
 
 class OurQGraphicsView(QGraphicsView):
@@ -18,3 +20,30 @@ class OurQGraphicsView(QGraphicsView):
 
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+    def middleMouseButtonPress(self, event):
+        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+                                   Qt.LeftButton, Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(releaseEvent)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
+        super().mousePressEvent(fakeEvent)
+
+    def middleMouseButtonRelease(self, event):
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() & -Qt.LeftButton, event.modifiers())
+        super().mousePressEvent(fakeEvent)
+        self.setDragMode(QGraphicsView.NoDrag)
